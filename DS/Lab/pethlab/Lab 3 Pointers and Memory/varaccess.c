529b55a6c4d0563952a0267cc4367c26ec8d722d
sakshi4719 <116568621+sakshi4719@users.noreply.github.com>
2025-01-13 22:31:20 +0530

    Delete DS/Lab/pethlab directory

--- DS/Lab/pethlab/Lab 3 Pointers and Memory/varaccess.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num2[] = {545,67,78,324};
-    for(int i = 0; i < 4; i++){
-        printf("VALUE %d", num2[i]);
-        printf("\tADDRESS %u\n", &num2[i]);
-    }
-}
